62c3ee2f8e03a3c8b80b859f93b3fc0d6bc7f841
HafizShaheed <shaheedkhan336@gmail.com>
2024-02-08 13:02:29 +0500

    12-28-23-12:02

--- config/database.php
+++ config/database.php
@@ -43,15 +43,16 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'url' => null,
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'databaseIS',
+            'username' => 'root',
+            'password' => '',
+            'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -59,9 +60,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => null, // You can hardcode other options here if needed
             ]) : [],
         ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
